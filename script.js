c7e5a908e20cb261d8b50abe8d402ba56ffb48c9
Leandrongr26 <marcos.leandrodn@outlook.com>
2022-02-03 11:59:22 -0300

    alteração dia 3

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ document.querySelector(".hamburguer").addEventListener("click", () =>
 );
 
 document.querySelector("#qtde").addEventListener("change", atualizarPreco)
-document.querySelector("#js").addEventListener("change", atualizarPreco)
-document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
-document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#tl").addEventListener("change", atualizarPreco)
+document.querySelector("#decor-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#decor-nao").addEventListener("change", atualizarPreco)
 document.querySelector("#prazo").addEventListener("change", function () {
     const prazo = document.querySelector("#prazo").value
     document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
@@ -14,13 +14,13 @@ document.querySelector("#prazo").addEventListener("change", function () {
 
 function atualizarPreco(){
     const qtde = document.querySelector("#qtde").value
-    const temJS = document.querySelector("#js").checked
-    const incluiLayout = document.querySelector("#layout-sim").checked
+    const temJS = document.querySelector("#tl").checked
+    const incluidecor = document.querySelector("#decor-sim").checked
     const prazo = document.querySelector("#prazo").value
   
     let preco = qtde * 100;
     if (temJS) preco *= 1.1
-    if (incluiLayout) preco += 500
+    if (incluidecor) preco += 500
     let taxaUrgencia = 1 - prazo*0.1;
     preco *= 1 + taxaUrgencia
    
